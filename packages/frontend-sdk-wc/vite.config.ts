e4a51b9915badd9f29f06586243edb6b7127dceb
wangchengwei0122 <892630kux@gmail.com>
2025-11-27 20:06:53 +0800

    styles: frontend-sdk-wc chat-widget styles

--- packages/frontend-sdk-wc/vite.config.ts
+++ packages/frontend-sdk-wc/vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
+    target: 'esnext',                // ⭐ 确保不降级 class、HTML element，自定义组件能工作
+    minify: false,
     lib: {
       entry: 'src/index.ts',
       name: 'QccAiChatbot',
       formats: ['es', 'umd'],
+      
       fileName: (format) =>
         format === 'es' ? 'qcc-ai-chatbot.js' : 'qcc-ai-chatbot.umd.js',
     },
